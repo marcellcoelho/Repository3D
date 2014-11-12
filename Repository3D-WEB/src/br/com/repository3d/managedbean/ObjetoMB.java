9f9e9cbbc463e73cf23a8f1ca1ed3339f98e14bb
Hosanam <Hosanam@Hosanam-HP>
2014-11-12 20:12:35 -0300

    Ajustes.

--- Repository3D-WEB/src/br/com/repository3d/managedbean/ObjetoMB.java
+++ Repository3D-WEB/src/br/com/repository3d/managedbean/ObjetoMB.java
@@ -1,13 +1,8 @@
 package br.com.repository3d.managedbean;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
